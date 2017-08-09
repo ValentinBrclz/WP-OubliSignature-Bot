78660232d1c691483335921297cce760befff349
Valentin Berclaz <ValentinBrclz@users.noreply.github.com>
2017-08-09 17:21:42 +0200

    Fix #16 - "there is a job named 'signature-manquante' already active "

--- run.sh
+++ run.sh
@@ -8,5 +8,8 @@ git pull
 # Install any new package
 npm install --production
 
+# Wait 10 seconds for process to finalise
+sleep 10
+
 # Start with forever
 jstart -mem 2g -N signature-manquante -cwd /shared/bin/node  lib/bot.js
