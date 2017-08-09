3a0e687124c8a6ffcd0231995ac77b52fe749ecc
Valentin Berclaz <ValentinBrclz@users.noreply.github.com>
2017-08-09 18:12:42 +0200

    Wait only if the bot was running

--- run.sh
+++ run.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 # Stop the bot
-jstop signature-manquante
+if jstop signature-manquante
+then
+   # Wait 10 seconds for process to finalise
+        echo "Waiting for previous job to finish..."
+        sleep 10
+fi
 
 # Get the last version
 git pull
 
 # Install any new package
 npm install --production
-
-# Wait 10 seconds for process to finalise
-echo "Waiting for previous job to finish..."
-sleep 10
 
 # Start with forever
 jstart -mem 2g -N signature-manquante -cwd /shared/bin/node  lib/bot.js
